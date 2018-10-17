481eeddc450600570e48e3e7476004ffc9ef5879
Steven <dungpn1105@hotmail.com>
2018-10-17 09:09:17 +0700

    Add bai moi

---
+++ Basic Syntax/If elif and else.py
@@ -0,0 +1,18 @@
+__author__="Dung"
+score = int(input("Nhap so diem: "))
+for i in range(score):
+    if score >= 90:
+        print ("Grade is A")
+    elif score >=80:
+        print ("Grade is B")
+    elif score >=70:
+        print ("Grade is C")
+    elif score >=60:
+        print ("Grade is D")
+    else:
+        print ('Grade is F')
+else:
+    print ("Goodbye")
+
+
+
